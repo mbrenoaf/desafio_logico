b559a1860a3ee3b98ce00b0cede33fd16d9e9b5d
Marcos Breno <123556594+mbrenoaf@users.noreply.github.com>
2023-02-15 15:44:34 -0300

    Add files via upload

---
+++ desafio_logico.js
@@ -0,0 +1,14 @@
+// Duas expressões resultar em TRUE
+let v = 1 + 1 < 3 && 2 / 2 == 1
+let v2 = 1 + 1 > 3 || 2 * 4 !== 7
+console.log(v)
+console.log(v2)
+
+// Duas expressões resultar em FALSE
+let v3 = 1 + 1 < 3 && false !== false
+let v4 = true !== true || 2 * 4 == 7
+console.log(v3)
+console.log(v4)
+
+
+
